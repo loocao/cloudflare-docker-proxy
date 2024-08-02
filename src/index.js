745cc9403fc1cd9978a4c5c25d6c17e1f06215c6
loocao <loocao@yeah.net>
2024-08-02 11:03:24 +0800

    替换域名

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.cf.loocao.cloudns.org": dockerHub,
+  "quay.cf.loocao.cloudns.org": "https://quay.io",
+  "gcr.cf.loocao.cloudns.org": "https://gcr.io",
+  "k8s-gcr.cf.loocao.cloudns.org": "https://k8s.gcr.io",
+  "k8s.cf.loocao.cloudns.org": "https://registry.k8s.io",
+  "ghcr.cf.loocao.cloudns.org": "https://ghcr.io",
+  "cloudsmith.cf.loocao.cloudns.org": "https://docker.cloudsmith.io",
+  "ecr.cf.loocao.cloudns.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cf.loocao.cloudns.org": dockerHub,
 };
 
 function routeByHosts(host) {
